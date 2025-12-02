73c1930e25ce2752e2bbda89845907b8c3def614
harshinisr153 <harshini@rewardz.sg>
2025-12-02 16:50:14 +0530

    - Upgraded to angular 19

--- projects/core-components/src/lib/app-text/app-text.component.ts
+++ projects/core-components/src/lib/app-text/app-text.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'lib-app-text',
-  templateUrl: './app-text.component.html',
-  styleUrls: ['./app-text.component.scss']
+    selector: 'lib-app-text',
+    templateUrl: './app-text.component.html',
+    styleUrls: ['./app-text.component.scss'],
+    standalone: false
 })
 export class AppTextComponent {
 
