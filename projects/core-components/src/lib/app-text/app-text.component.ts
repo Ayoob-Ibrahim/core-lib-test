de5ae1a5c594369feb1855e752603d9ec0558111
harshinisr153 <harshini@rewardz.sg>
2025-12-02 16:57:01 +0530

    - Added angular material - Added app-text component

--- projects/core-components/src/lib/app-text/app-text.component.ts
+++ projects/core-components/src/lib/app-text/app-text.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-    selector: 'lib-app-text',
-    templateUrl: './app-text.component.html',
-    styleUrls: ['./app-text.component.scss'],
-    standalone: false
+  selector: 'app-text',
+  templateUrl: './app-text.component.html',
+  styleUrl: './app-text.component.scss',
+  standalone: false,
 })
 export class AppTextComponent {
+  @Input() type: string = 'body';
+  @Input() size: string = 'large';
+  @Input() fontSize: string | undefined = undefined;
+  @Input() bold: boolean = false;
+  @Input() semiBold: boolean = false;
+  @Input() removeMargin: boolean = false;
+  @Input() textColor!: string;
+  @Input() class!: string;
+  @Input() htmlContent!: string;
 
+  get textClass() {
+    let classes = `mat-${this.type}-${this.size}`;
+    if (this.bold) {
+      classes += ' font-bold';
+    }
+    if (this.semiBold) {
+      classes += ' font-semi-bold';
+    }
+    if (this.removeMargin) {
+      classes += ' m-0';
+    }
+    if (this.class) {
+      classes += ` ${this.class}`;
+    }
+    return classes;
+  }
 }
